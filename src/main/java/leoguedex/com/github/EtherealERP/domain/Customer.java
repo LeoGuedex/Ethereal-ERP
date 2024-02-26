1120e2b41d1d7216cecfd6ec229c15a1a988ca8f
Leo Guedes <leoguedesdev@gmail.com>
2024-02-26 20:40:30 -0300

    Melhoria de clean code

--- src/main/java/leoguedex/com/github/EtherealERP/domain/Customer.java
+++ src/main/java/leoguedex/com/github/EtherealERP/domain/Customer.java
@@ -1,6 +1,10 @@
 package leoguedex.com.github.EtherealERP.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import leoguedex.com.github.EtherealERP.domain.enums.ExpectedEnum;
 import leoguedex.com.github.EtherealERP.domain.enums.IndicatedByEnum;
 import lombok.AllArgsConstructor;
@@ -15,37 +19,37 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Customer {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    private String name;
-    private Integer age;
-    private String email;
-    private Double weight;
-    private Double height;
-    private String address;
-    private String birthDate;
-    private String phoneNumber;
-    private Boolean worksOrStudies;
-    private String consultationReason;
-    private String whenCreated;
-    private IndicatedByEnum referredBy;
-    private ExpectedEnum expectedOutcome;
+  private String name;
+  private Integer age;
+  private String email;
+  private Double weight;
+  private Double height;
+  private String address;
+  private String birthDate;
+  private String phoneNumber;
+  private Boolean worksOrStudies;
+  private String consultationReason;
+  private String whenCreated;
+  private IndicatedByEnum referredBy;
+  private ExpectedEnum expectedOutcome;
 
-    @OneToOne(mappedBy = "customer")
-    private CustomerAlimentation customerAlimentation;
+  @OneToOne(mappedBy = "customer")
+  private CustomerAlimentation customerAlimentation;
 
-    @OneToOne(mappedBy = "customer")
-    private CustomerGym customerGym;
+  @OneToOne(mappedBy = "customer")
+  private CustomerGym customerGym;
 
-    @OneToOne(mappedBy = "customer")
-    private CustomerHealth customerHealth;
+  @OneToOne(mappedBy = "customer")
+  private CustomerHealth customerHealth;
 
-    @OneToOne(mappedBy = "customer")
-    private CustomerNutritionistInformation customerNutritionistInformation;
+  @OneToOne(mappedBy = "customer")
+  private CustomerNutritionistInformation customerNutritionistInformation;
 
-    @OneToOne(mappedBy = "customer")
-    private CustomerRotineTable customerRotineTable;
+  @OneToOne(mappedBy = "customer")
+  private CustomerRotineTable customerRotineTable;
 
 }
